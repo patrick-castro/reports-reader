1afc1dc92805f81bcf7628e142c6a36aae52ad10
patrick-castro <castropatrickantonio@gmail.com>
2023-06-18 14:42:06 +0800

    Add constants

---
+++ src/components/XLSXRenderer/constants.ts
@@ -0,0 +1,63 @@
+const columns = [
+  'AGENDA',
+  'SPECIFIC SUBJECT MATTER / DETAILS',
+  'RESULTS/ PROPOSED ACTION/ ACTIONS TAKEN/ COMMENTS',
+  'TARGET DATE',
+  'PERSON RESPONSIBLE',
+  'ACTUAL COMPLETION DATE',
+]
+
+const prodCostContainment = [
+  ['', 'Productivity (kg/mnhr)'],
+  ['', 'Process Reliability'],
+  ['', 'Line 1'],
+  ['', 'Line 2'],
+  ['', 'Line 3'],
+  ['', 'Line 4'],
+  ['', 'Power, kWH per kg noodle'],
+  ['', 'Scrap to FG Ratio'],
+  ['', 'Product Returns'],
+  ['', 'Implementation of Preventive Maintenance Work Order System'],
+  ['', 'Steam Utilization (kg steam/kg product)'],
+  ['', 'Waste Oil Generation (L)'],
+  ['', 'Disabling Safety Incident'],
+  ['', 'Non-Disabling Safety Incident'],
+]
+
+const engCostContainment = [
+  ['', 'Power Cost'],
+  ['', 'Fuel Cost'],
+  ['', 'Implementation of Preventive Maintenance Work Order System'],
+  ['', 'Fuel Utilization (kg steam / kg coal'],
+  ['', 'Disabling Safety Incident'],
+  ['', 'Non-Disabling Safety Incident'],
+]
+
+const custCareProd = [['', 'Manpower Attendance Rate']]
+
+const costCareProdMatLogEng = [['', 'MSA (Master Schedule Adherence)']]
+
+const custCareQA = [
+  [
+    '',
+    'Support product withdrawal reduction of occurrence:\nConduct timely verification of parameters thus will not contribute to withdrawal.',
+  ],
+  [
+    '',
+    'Support timeliness of submission & closure of CPIAR by conducting timely verification  and timely reporting of status of CPIARs of respective customers.',
+  ],
+  [
+    '',
+    'Support Short Count minimization:\nConduct on time verification of short count based on specified frequency.',
+  ],
+  [
+    '',
+    'Support product weight conformance:\n1. Conduct on time verification of noodle weight based on specified frequency.\n2. Timely reporting of result of noodle weight monitoring to customer.\n3. Use Aster in reporting.',
+  ],
+  ['', 'Maintain Certification. No Major/Critical NC in External Audit'],
+  ['', 'Annual Proficiency Test'],
+  ['', 'Disabling Safety Incident'],
+  ['', 'Non-Disabling Safety Incident'],
+]
+
+const ppleCareHR = [['', 'Developmental Trainings ']]
