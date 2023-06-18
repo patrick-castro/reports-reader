4c5e6d476cfe73a03854022b14018a603f7e57cf
patrick-castro <castropatrickantonio@gmail.com>
2023-06-18 16:10:23 +0800

    Add file upload feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
+import React, { useCallback, useState } from 'react'
+import { read, utils } from 'xlsx'
+
 import './App.css'
 
 function App() {
+  const [items, setItems] = useState([])
+
+  const readExcel = useCallback((file: File) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader()
+      fileReader.readAsArrayBuffer(file)
+
+      fileReader.onload = (e: ProgressEvent<FileReader>) => {
+        if (!e.target) return
+
+        const bufferArray = e.target.result
+
+        const workbook = read(bufferArray, { type: 'buffer' })
+
+        const wsname = workbook.SheetNames[0]
+
+        const worksheet = workbook.Sheets[wsname]
+
+        const data = utils.sheet_to_json(worksheet)
+
+        resolve(data)
+      }
+
+      fileReader.onerror = (error) => {
+        reject(error)
+      }
+    })
+
+    promise.then((res: any) => {
+      setItems(res)
+    })
+  }, [])
+
+  const onChangeFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = (e?.target?.files || [])[0]
+    if (!file) return
+
+    readExcel(file)
+  }
+
   return (
     <main>
-      <button className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-        Extract
-      </button>
+      <div>
+        <input type='file' onChange={onChangeFileInput} />
+      </div>
     </main>
   )
 }
