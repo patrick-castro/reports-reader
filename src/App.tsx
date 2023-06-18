20c86e269a204166aa2378e42f2c53ef596842df
patrick-castro <castropatrickantonio@gmail.com>
2023-06-18 12:26:36 +0800

    Add Extract button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css'
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <main>
+      <button className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+        Extract
+      </button>
+    </main>
+  )
 }
 
 export default App
